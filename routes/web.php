2fcba7fbdbdad7679c4a04c4ef9e9e2b5dc74eb8
RafaHaris <rafacrimson2@gmail.com>
2025-11-27 23:50:47 +0800

    Dashboard Done

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,18 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Apps\DashboardController;
 
 Route::get('/', function(){
     if(Auth::check())
         return to_route('apps.dashboard');
 
     return inertia('auth/login');
+});
+
+Route::group(['prefix' => 'apps', 'as' => 'apps.', 'middleware' => ['auth']], function(){
+    // dashboard route
+    Route::get('/dashboard', DashboardController::class)->name('dashboard');
 });
 
 
