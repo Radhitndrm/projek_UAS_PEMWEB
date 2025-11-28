c612c2f474d04bac9f0f6430dad7cbffc0cafa3c
Radhitndrm <radhit.andromeda@gmail.com>
2025-11-28 13:29:21 +0800

    roles done

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,22 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Apps\DashboardController;
+use App\Http\Controllers\Apps\RoleController;
 
-Route::get('/', function(){
-    if(Auth::check())
+Route::get('/', function () {
+    if (Auth::check())
         return to_route('apps.dashboard');
 
     return inertia('auth/login');
 });
 
-Route::group(['prefix' => 'apps', 'as' => 'apps.', 'middleware' => ['auth']], function(){
+Route::group(['prefix' => 'apps', 'as' => 'apps.', 'middleware' => ['auth']], function () {
     // dashboard route
     Route::get('/dashboard', DashboardController::class)->name('dashboard');
+
+    Route::resource('roles', RoleController::class)->except('show');
 });
 
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
